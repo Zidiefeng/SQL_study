fa39a34224aac432ddc1e681c66f0be05634f9dd
Zidiefeng <sunkaitan123@163.com>
2021-05-04 23:06:59 -0400

    update dql

--- code/SQL_kuang/school_dql.sql
+++ code/SQL_kuang/school_dql.sql
@@ -33,3 +33,77 @@ SELECT @@auto_increment_increment;
 
 -- grade +1
 SELECT `studentno`, `studentresult`+1 AS '提分后' FROM result;
+
+-- ---------------where
+USE school;
+
+-- and
+SELECT studentno, studentresult 
+FROM result
+WHERE studentresult>=95 AND studentresult <=100;
+
+-- &&
+SELECT studentno, studentresult 
+FROM result
+WHERE studentresult>=95 && studentresult <=100;
+
+-- between and 
+SELECT studentno, studentresult 
+FROM result
+WHERE studentresult BETWEEN 95 AND 100;
+
+-- !=
+SELECT studentno, studentresult 
+FROM result
+WHERE studentresult !=98;
+
+-- not
+SELECT studentno, studentresult 
+FROM result
+WHERE NOT studentresult =98;
+
+-- ----------------模糊查询like
+
+-- 赵某某
+SELECT `studentno`, `studentname`
+FROM `student`
+WHERE studentname LIKE '赵_';
+
+-- 赵某
+SELECT `studentno`, `studentname`
+FROM `student`
+WHERE studentname LIKE '赵__';
+
+-- 赵开头的
+SELECT `studentno`, `studentname`
+FROM `student`
+WHERE studentname LIKE '赵%';
+
+-- 名字中带有伟的
+SELECT `studentno`, `studentname`
+FROM `student`
+WHERE studentname LIKE '%伟%';
+
+-- ----------------模糊查询in
+-- 查询1000，1002，1003的学员
+SELECT `studentno`, `studentname`
+FROM `student`
+WHERE studentno IN (1000,1002,1003);
+
+-- 查询gradeid为1或2的科目
+SELECT `subjectname`, `classhour`, gradeid
+FROM `subject`
+WHERE gradeid IN (1,2);
+
+-- ----------------模糊查询null
+-- address为空
+SELECT studentno, studentname, address FROM student
+WHERE address = '' OR address IS NULL;
+
+-- 出生日期不为空
+SELECT studentno, studentname, borndate FROM student
+WHERE borndate IS NOT NULL;
+
+
+
+
